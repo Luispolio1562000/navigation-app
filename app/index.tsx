3bb12432707ec4d242606bb77229723ee27e4a9c
Luis Gustavo Polio Fernandez <luis.gustavo@qrsof.com>
2025-05-27 22:41:34 -0600

    First commit

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const App = () => {
+  return (
+    <View className="mt-10">
+      <Text className="text-blue-500 text-3xl text-center">index</Text>
+    </View>
+  );
+};
+
+export default App;
