3e982ddcdc3a8e9be8cf5c3709c357875d4f060f
Luis Gustavo Polio Fernandez <luis.gustavo@qrsof.com>
2025-06-05 22:33:06 -0600

    Implemented tabs

---
+++ app/tabs/_layout.tsx
@@ -0,0 +1,36 @@
+import { Ionicons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
+import React from "react";
+
+const TabsLayout = () => {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: "pink",
+        tabBarStyle: { backgroundColor: "#ffffff" },
+        tabBarShowLabel: false,
+      }}
+    >
+      <Tabs.Screen
+        name="home/index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            <Ionicons size={28} name="home" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="favorites/index"
+        options={{
+          title: "Favoritos",
+          tabBarIcon: ({ color }) => (
+            <Ionicons size={28} name="heart-outline" color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+};
+
+export default TabsLayout;
