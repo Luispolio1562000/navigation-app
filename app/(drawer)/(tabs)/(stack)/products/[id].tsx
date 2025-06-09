8244f092e7537ca49f2813336e4d045410f222b1
Luis Gustavo Polio Fernandez <luis.gustavo@qrsof.com>
2025-06-08 23:14:05 -0600

    Fix: changes in routing of id product details

--- app/(drawer)/(tabs)/(stack)/products/[id].tsx
+++ app/(drawer)/(tabs)/(stack)/products/[id].tsx
@@ -1,12 +1,20 @@
 import { products } from "@/store/products.store";
-import { Redirect, useLocalSearchParams } from "expo-router";
-import React from "react";
+import { Redirect, useLocalSearchParams, useNavigation } from "expo-router";
+import React, { useEffect } from "react";
 import { Text, View } from "react-native";
 
 const ProductDetailsScreen = () => {
+  const navigation = useNavigation();
   const { id } = useLocalSearchParams();
   const product = products.find((product) => product.id === id);
-  if (!product) return <Redirect href="/(stack)/products" />;
+
+  useEffect(() => {
+    navigation.setOptions({
+      title: product?.title ?? "Producto",
+    });
+  }, [product, navigation]);
+
+  if (!product) return <Redirect href="/(drawer)/(tabs)/(stack)/products" />;
   return (
     <View className="flex flex-col gap-4 p-4">
       <Text className="text-2xl font-inter-bold">{product.title}</Text>
