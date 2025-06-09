5353f6999d52a572c46859590a62fe9f5c1aaa36
Luis Gustavo Polio Fernandez <luis.gustavo@qrsof.com>
2025-06-08 20:32:10 -0600

    Change redirect in  app/index.tsx and change routing add () in folders

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const App = () => {
   //return <Redirect href="/(stack)/home" />;
-  return <Redirect href="/drawer" />;
+  return <Redirect href="/home" />;
 };
 
 export default App;
